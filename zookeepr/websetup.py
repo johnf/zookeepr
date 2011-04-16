e814fee253cae139243318f6015a8cbdc8df61a9
Joshua Hesketh <josh@nitrotech.org>
2011-04-17 00:15:08 +1000

    work on pylons1.0

--- zookeepr/websetup.py
+++ zookeepr/websetup.py
@@ -3,12 +3,16 @@ import logging
 
 from zookeepr.config.environment import load_environment
 from zookeepr.model import meta
+import pylons.test
 
 import zookeepr.model
 
 log = logging.getLogger(__name__)
 
 def setup_app(command, conf, vars):
+    # Don't reload the app if it was loaded under the testing environment
+    if not pylons.test.pylonsapp:
+        load_environment(conf.global_conf, conf.local_conf)
     """Place any commands to setup zookeepr here"""
     load_environment(conf.global_conf, conf.local_conf)
 
